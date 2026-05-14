783a86fc5592ecd61690c3314f9e3746c5825ead
a.kenzhebek1643 <a.kenzhebek1643@gmail.com>
2026-05-14 20:06:53 +0500

    chore: regenerate platform plugin registrants after pdf/printing add
    flutter pub add added pdf + printing plugins; these are the auto-
    generated platform glue files. macOS Podfile.lock pulls in the
    native printing plugin; Linux/Windows registrants list it.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_picker
 import path_provider_foundation
+import printing
 import shared_preferences_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
